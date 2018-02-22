ee808f091dbf12effcf8fe5e6822c3fc7ed8fa96
LUCRUZ <lucruz@hdu16055.gabril.com.br>
2018-02-22 14:49:35 -0300

    change for fetch

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,7 @@
 process.env.DEBUG = 'actions-on-google:*';
 const App = require('actions-on-google').DialogflowApp;
 const functions = require('firebase-functions');
-const https = require("node-fetch");
+const fetch = require("node-fetch");
 
 
 
@@ -27,24 +27,37 @@ exports.horoscope = functions.https.onRequest((request, response) => {
 
 
     let sign = app.getArgument(SIGN_ARGUMENT);
-    var horoscope_response = '';
-	
+    var horoscope_response = '';	
 
-	const url =
-	  "http://sandipbgt.com/theastrologer/api/horoscope/" + sign + "/today/";
-	fetch(url)
-	  .then(response => {
-	    response.json().then(json => {
-	    	horoscope_response = `${json.horoscope}`;
+ 	const url = "http://sandipbgt.com/theastrologer/api/horoscope/" + sign + "/today/";
+  
+	function callAPI(url) { 
+    	return fetch(url)
+	    .then((response) => {
+		   if(response.ok) {
+		     return response.json();
+		   } else {
+		     throw new Error('Server response wasn\'t OK');
+		   }
+	 	})
+	    .then((json) => {
+			const response_json = json.horoscope;  
+			return response_json ;
+		})  
+	    .catch(error => {
+			console.log(error);
 	    });
-	  })
-	  .catch(error => {
-	    console.log(error);
-	  });
+    }
+
+	horoscope_response = callAPI(url).then((response_json)=>{
+	      return response_json
+	});
+    
+    app.tell('Alright, horoscope for '+ sign +' today is:  ' + horoscope_response + '');
 
     
 
-    app.tell('Alright, horoscope for '+ sign +' today is:  ' + horoscope_response + '');
+    
   }
 
   // d. build an action map, which maps intent names to functions
--- functions/teste.js
+++ functions/teste.js
@@ -1,18 +1,38 @@
 const sign = 'aquarius';
-const https = require("http");
+const fetch = require("node-fetch");
 
-const url =
-  "http://sandipbgt.com/theastrologer/api/horoscope/" + sign + "/today/";
-https.get(url, res => {
-  res.setEncoding("utf8");
-  let body = "";
-  res.on("data", data => {
-    body += data;
-  });
-  res.on("end", () => {
-    body = JSON.parse(body);
-    console.log(
-      `${body.horoscope}`
-    );
-  });
-});
+
+    
+    var horoscope_response = '';
+  
+
+  const url = "http://sandipbgt.com/theastrologer/api/horoscope/" + sign + "/today/";
+  
+  function callAPI(url) { 
+    return fetch(url)
+    .then((response) => {
+   if(response.ok) {
+     return response.json();
+   } else {
+     throw new Error('Server response wasn\'t OK');
+   }
+ })
+    .then((json) => {
+   const response_json = json.horoscope;  
+   return response_json ;
+ })
+
+     
+      .catch(error => {
+       console.log(error);
+      });
+
+    }
+
+horoscope_response = callAPI(url).then((response_json)=>{
+      return response_json;
+    });
+
+
+console.log(horoscope_response);
+    
